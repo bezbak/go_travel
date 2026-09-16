c28b0f3984ec65128c94d3c45d4e6ad6578de7bd
bezbak <Cemetey.2019@gmail.com>
2026-09-16 19:51:54 +0600

    faesf

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", ["localhost", "127.0.0.1", "[::1]"]).split(',')
+ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", ["localhost", "127.0.0.1", "[::1]"])
 
 INSTALLED_APPS = [
     # Must precede django.contrib.admin so Jazzmin can override its templates.
